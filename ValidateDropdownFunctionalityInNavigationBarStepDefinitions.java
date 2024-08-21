a6733994cde5e10f44d05b5e333ae5e868c368ea
Anastasiia Udovychenko <anastasiia_udovychenko@epam.com>
2024-08-21 17:29:03 -0400

    Create ValidateDropdownFunctionalityInNavigationBarStepDefinitions.java

---
+++ ValidateDropdownFunctionalityInNavigationBarStepDefinitions.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ValidateDropdownFunctionalityInNavigationBarStepDefinitions {
+
+    WebDriver driver;
+
+    @FindBy(id = "navigation-bar")
+    WebElement navigationBar;
+
+    public ValidateDropdownFunctionalityInNavigationBarStepDefinitions(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    // Add step definitions here
+}
