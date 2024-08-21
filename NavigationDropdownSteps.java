423ec3a7ed068dd61b13068facc61a106f4b49b9
Anastasiia Udovychenko <anastasiia_udovychenko@epam.com>
2024-08-21 17:17:17 -0400

    Create NavigationDropdownSteps.java

---
+++ NavigationDropdownSteps.java
@@ -0,0 +1,10 @@
+package stepdefinitions;
+
+import pageobjects.MainPage;
+import org.junit.jupiter.api.Assertions;
+
+public class NavigationDropdownSteps {
+
+    // Step definitions for the scenarios in the feature file will go here
+
+}
