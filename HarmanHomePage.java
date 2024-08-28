7a318b6ff8af200b39947c35eb54bbe7b2141e9e
Anastasiia Udovychenko <anastasiia_udovychenko@epam.com>
2024-08-28 01:34:05 -0400

    Create HarmanHomePage.java

---
+++ HarmanHomePage.java
@@ -0,0 +1,26 @@
+// Page Object for Harman Home Page
+
+public class HarmanHomePage {
+
+  // Locator for Innovations dropdown
+  private By innovationsDropdown = By.id("innovations-dropdown");
+
+  public void clickOnInnovationsDropdown() {
+    // Code to click on Innovations dropdown
+  }
+
+  // Method to verify if dropdown is open
+  public boolean isDropdownOpen() {
+    // Code to check if dropdown is open
+  }
+
+  // Method to click outside of the dropdown
+  public void clickOutsideOfDropdown() {
+    // Code to simulate a click outside of the dropdown
+  }
+
+  // Method to click on another navigation item
+  public void clickOnAnotherNavigationItem() {
+    // Code to click on another navigation item
+  }
+}
