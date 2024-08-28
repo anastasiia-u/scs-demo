585b7b6aecbd7f0671ba0be1c08a975f9f42b699
Anastasiia Udovychenko <anastasiia_udovychenko@epam.com>
2024-08-28 01:33:57 -0400

    Create ValidateInnovationsDropdown.java

---
+++ ValidateInnovationsDropdown.java
@@ -0,0 +1,49 @@
+// Step Definitions for Validate Innovations Dropdown Functionality
+
+public class ValidateInnovationsDropdown {
+
+  @Given("^I am on the homepage$")
+  public void i_am_on_the_homepage() {
+    // Code to navigate to the homepage
+  }
+
+  @When("^I click on the Innovations dropdown$")
+  public void i_click_on_the_innovations_dropdown() {
+    // Code to click on the Innovations dropdown
+  }
+
+  @Then("^I should see all necessary options$")
+  public void i_should_see_all_necessary_options() {
+    // Code to verify the options
+  }
+
+  @When("^I click on an option in the Innovations dropdown$")
+  public void i_click_on_an_option_in_the_innovations_dropdown() {
+    // Code to click on an option
+  }
+
+  @Then("^I should be navigated to the corresponding page$")
+  public void i_should_be_navigated_to_the_corresponding_page() {
+    // Code to navigate
+  }
+
+  @When("^I click outside of the dropdown$")
+  public void i_click_outside_of_the_dropdown() {
+    // Code to click outside
+  }
+
+  @Then("^the dropdown should close$")
+  public void the_dropdown_should_close() {
+    // Code to close the dropdown
+  }
+
+  @When("^I click on another navigation item$")
+  public void i_click_on_another_navigation_item() {
+    // Code to click on another item
+  }
+
+  @Then("^the dropdown should close$")
+  public void the_dropdown_should_also_close() {
+    // Code to ensure the dropdown closes
+  }
+}
