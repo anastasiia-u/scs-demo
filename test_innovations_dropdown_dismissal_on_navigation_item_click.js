f595ddf8d5b86995804762f478596e1b5e0375ec
Anastasiia Udovychenko <anastasiia_udovychenko@epam.com>
2024-09-05 16:32:50 -0400

    Create test_innovations_dropdown_dismissal_on_navigation_item_click.js

---
+++ test_innovations_dropdown_dismissal_on_navigation_item_click.js
@@ -0,0 +1,5 @@
+describe('Innovations dropdown interaction with other navigation items', () => {
+  it('should close the Innovations dropdown menu when interacting with other navigation items', () => {
+    // Test implementation for dropdown dismissal on navigation item click
+  });
+});
