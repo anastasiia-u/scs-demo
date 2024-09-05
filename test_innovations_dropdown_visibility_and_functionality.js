1332f7fbf911cfe5075118c0527c05d4900b925e
Anastasiia Udovychenko <anastasiia_udovychenko@epam.com>
2024-09-05 16:32:41 -0400

    Create test_innovations_dropdown_visibility_and_functionality.js

---
+++ test_innovations_dropdown_visibility_and_functionality.js
@@ -0,0 +1,5 @@
+describe('Innovations dropdown', () => {
+  it('should be visible and functional, navigating to correct pages on option click', () => {
+    // Test implementation for visibility and functionality
+  });
+});
