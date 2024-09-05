d23d861a0cfcf63e251adb4fd4b98069dd461111
Anastasiia Udovychenko <anastasiia_udovychenko@epam.com>
2024-09-05 16:32:45 -0400

    Create test_innovations_dropdown_dismissal_on_external_click.js

---
+++ test_innovations_dropdown_dismissal_on_external_click.js
@@ -0,0 +1,5 @@
+describe('Innovations dropdown dismissal', () => {
+  it('should dismiss correctly when clicking outside of the dropdown menu', () => {
+    // Test implementation for dropdown dismissal on external click
+  });
+});
