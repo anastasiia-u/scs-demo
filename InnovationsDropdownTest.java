21069da0127de98ce22a77399a8fd6a57716cee8
Anastasiia Udovychenko <anastasiia_udovychenko@epam.com>
2024-09-05 19:04:23 -0400

    Create InnovationsDropdownTest.java

---
+++ InnovationsDropdownTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class InnovationsDropdownTest {
+
+    @Test
+    public void testInnovationsDropdown() {
+        // Test steps for 'Innovations' dropdown functionality
+    }
+}
