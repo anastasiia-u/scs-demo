20e623197ea11f662730a4637423f84ce7988d4d
Anastasiia Udovychenko <anastasiia_udovychenko@epam.com>
2024-09-12 16:17:14 -0400

    Update src/main/java/pageobjects/MainPage.java

--- src/main/java/pageobjects/MainPage.java
+++ src/main/java/pageobjects/MainPage.java
@@ -5,6 +5,13 @@ package pageobjects;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
     
-public class MainPage extends BasePage {
+package pageobjects;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import java.util.List;
+
+public class MainPage extends BasePage {
+    // Selenium WebElements and actions for the 'Solutions' dropdown.
+    // Elements and methods to interact with the Solutions dropdown will be added here.
 }
