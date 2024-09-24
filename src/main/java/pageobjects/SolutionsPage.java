51b77ccf1d5cdae939f53d002bb6e23582c6913a
Anastasiia Udovychenko <anastasiia_udovychenko@epam.com>
2024-09-24 16:47:45 -0400

    Create src/main/java/pageobjects/SolutionsPage.java

---
+++ src/main/java/pageobjects/SolutionsPage.java
@@ -0,0 +1,60 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.List;
+
+public class SolutionsPage extends BasePage {
+
+    private String url = "https://www.harman.com";
+
+    // Locator for the "Solutions" dropdown menu
+    @FindBy(id = "navlink-_l2")
+    private WebElement solutionsDropdown;
+
+    // Locators for the options within the "Solutions" dropdown
+    @FindBy(xpath = "//a[@href='/automotive']")
+    private WebElement automotiveOption;
+
+    @FindBy(xpath = "//span[@id='tabnav-_l38']")
+    private WebElement lifestyleOption;
+
+    @FindBy(xpath = "//a[@href='/lifestyle']")
+    private WebElement consumerOption;
+
+    @FindBy(xpath = "//a[@href='https://pro.harman.com']")
+    private WebElement professionalOption;
+
+    @FindBy(xpath = "//a[@href='https://services.harman.com']")
+    private WebElement digitalTransformationOption;
+
+    // Constructor
+    public SolutionsPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    // Method to open the page
+    public void open() {
+        driver.get(url);
+    }
+
+    // Method to click on the "Solutions" dropdown
+    public void clickSolutionsDropdown() {
+        wait.until(ExpectedConditions.elementToBeClickable(solutionsDropdown));
+        solutionsDropdown.click();
+    }
+
+    // Method to verify visibility of dropdown options
+    public boolean areDropdownOptionsVisible() {
+        wait.until(ExpectedConditions.visibilityOf(automotiveOption));
+        return automotiveOption.isDisplayed() && lifestyleOption.isDisplayed() &&
+               consumerOption.isDisplayed() && professionalOption.isDisplayed() &&
+               digitalTransformationOption.isDisplayed();
+    }
+
+    // Method to click on the "Automotive" option
+    public void clickAutomotiveOption() {
+        wait.until(ExpectedConditions.element
