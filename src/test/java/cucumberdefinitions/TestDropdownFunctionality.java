e7d3844e850b1177dc726a96dd82e651fc51e9c3
Anastasiia Udovychenko <anastasiia_udovychenko@epam.com>
2024-10-22 09:17:31 -0400

    Create src/test/java/cucumberdefinitions/TestDropdownFunctionality.java

---
+++ src/test/java/cucumberdefinitions/TestDropdownFunctionality.java
@@ -0,0 +1,74 @@
+package cucumberdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import pageobjects.MainPage;
+import pageobjects.SolutionsPage;
+import org.junit.jupiter.api.Assertions;
+
+public class TestDropdownFunctionality {
+    MainPage mainPage;
+    SolutionsPage solutionsPage;
+
+    public TestDropdownFunctionality() {
+        mainPage = new MainPage();
+        solutionsPage = new SolutionsPage();
+    }
+
+    @Given("I am on the main navigation bar")
+    public void i_am_on_the_main_navigation_bar() {
+        mainPage.open();
+    }
+
+    @When("I click on the {string} dropdown menu")
+    public void i_click_on_the_dropdown_menu(String dropdownName) {
+        mainPage.getDropdownMenu(dropdownName).click();
+    }
+
+    @Then("I should see options such as {string}, {string}, {string}, {string}, and {string}")
+    public void i_should_see_options(String option1, String option2, String option3, String option4, String option5) {
+        Assertions.assertTrue(mainPage.isOptionVisible(option1));
+        Assertions.assertTrue(mainPage.isOptionVisible(option2));
+        Assertions.assertTrue(mainPage.isOptionVisible(option3));
+        Assertions.assertTrue(mainPage.isOptionVisible(option4));
+        Assertions.assertTrue(mainPage.isOptionVisible(option5));
+    }
+
+    @Given("the {string} dropdown menu is open")
+    public void the_dropdown_menu_is_open(String dropdownName) {
+        mainPage.getDropdownMenu(dropdownName).click();
+        Assertions.assertTrue(mainPage.isDropdownOpen(dropdownName));
+    }
+
+    @When("I click on the {string} option")
+    public void i_click_on_the_option(String optionName) {
+        mainPage.getOption(optionName).click();
+    }
+
+    @Then("I should be navigated to the {string} section of the website")
+    public void i_should_be_navigated_to_the_section_of_the_website(String sectionName) {
+        Assertions.assertTrue(solutionsPage.isAtSection(sectionName));
+    }
+
+    @When("I click outside the dropdown menu")
+    public void i_click_outside_the_dropdown_menu() {
+        mainPage.clickOutsideDropdown();
+    }
+
+    @Then("the dropdown should close")
+    public void the_dropdown_should_close() {
+        Assertions.assertFalse(mainPage.isDropdownOpen());
+    }
+
+    @When("I attempt to click on a non-existent option")
+    public void i_attempt_to_click_on_a_non_existent_option() {
+        mainPage.clickNonExistentOption();
+    }
+
+    @Then("no navigation should occur and the dropdown should remain open")
+    public void no_navigation_should_occur_and_the_dropdown_should_remain_open() {
+        Assertions.assertTrue(mainPage.isDropdownOpen());
+        Assertions.assertFalse(solutionsPage.isNavigationOccurred());
+    }
+}
